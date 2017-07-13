bb523af3b6bf46f29ebfdc52155c2567ee8a171e
John Haugeland <stonecypher@gmail.com>
2017-07-12 17:10:01 -0700

    initial publish, nearly empty

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    rules: {
+    	"for-direction"    : 0,
+    	"getter-return"    : "error",
+    	"no-await-in-loop" : "warning",
+        "semi"             : ["error", "always"]
+    }
+
+};
