174a2d55cc6368a1831d3e7af00f96aa15102425
John Haugeland <stonecypher@gmail.com>
2017-07-12 17:18:41 -0700

    bump, enable a few more rules, set missing keyword

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 module.exports = {
 
     rules: {
-    	"for-direction"    : 0,
-    	"getter-return"    : "error",
-    	"no-await-in-loop" : "warning",
-        "semi"             : ["error", "always"]
+    	"for-direction"         : 0,
+    	"getter-return"         : "error",
+    	"no-await-in-loop"      : "warn",
+    	"no-compare-neg-zero"   : "warn",
+    	"no-cond-assign"        : ["warn", "except-parens"],
+    	"no-console"            : "warn",
+    	"no-constant-condition" : "warn",
+    	"no-control-regex"      : "warn",
+        "semi"                  : ["error", "always"]
     }
 
 };
