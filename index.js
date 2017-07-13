098c8cb32d571a157dc4536a29a7bf67de21d92c
John Haugeland <stonecypher@gmail.com>
2017-07-12 17:25:14 -0700

    bump, enable more rules

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 module.exports = {
 
     rules: {
-    	"for-direction"         : 0,
-    	"getter-return"         : "error",
-    	"no-await-in-loop"      : "warn",
-    	"no-compare-neg-zero"   : "warn",
-    	"no-cond-assign"        : ["warn", "except-parens"],
-    	"no-console"            : "warn",
-    	"no-constant-condition" : "warn",
-    	"no-control-regex"      : "warn",
-        "semi"                  : ["error", "always"]
+        "for-direction"            : 0,
+        "getter-return"            : "error",
+        "no-await-in-loop"         : "warn",
+        "no-compare-neg-zero"      : "warn",
+        "no-cond-assign"           : ["warn", "except-parens"],
+        "no-console"               : "warn",
+        "no-constant-condition"    : "warn",
+        "no-control-regex"         : "warn",
+        "no-debugger"              : "warn",
+        "no-dupe-args"             : "error",
+        "no-dupe-keys"             : "error",
+        "no-duplicate-case"        : "error",
+        "no-empty"                 : "warn",
+        "no-empty-character-class" : "error",
+        "no-ex-assign"             : "error",
+        "no-extra-boolean-cast"    : "warn",
+        "no-extra-parens"          : 0,
+        "no-extra-semi"            : "warn",
+        "no-func-assign"           : "error",
+        "no-inner-declarations"    : "error",
+        "semi"                     : ["error", "always"]
     }
 
 };
